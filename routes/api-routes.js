be4f23df02f3298791986490573969ca66d609b2
JoeDodgson <j.dodgson@live.co.uk>
2020-07-22 15:00:45 +0100

    Added a .post(/api/workouts/) route

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -32,22 +32,22 @@ module.exports = (app) => {
             if (type === "resistance") {
                 const { name, duration, weight, reps, sets } = req.body;
                 exercises.push({
-                        _id: id,
-                        type,
-                        name,
-                        duration,
-                        weight,
-                        reps,
-                        sets
+                    _id: id,
+                    type,
+                    name,
+                    duration,
+                    weight,
+                    reps,
+                    sets
                 });
             } else if (type === "cardio") {
                 const { name, distance, duration } = req.body;
                 exercises.push({
-                        _id: id,
-                        type,
-                        name,
-                        distance,
-                        duration
+                    _id: id,
+                    type,
+                    name,
+                    distance,
+                    duration
                 });
             }
 
@@ -62,6 +62,52 @@ module.exports = (app) => {
 
         } catch (err) {
             console.error(`ERROR - api-routes.js - .put('/api/workouts/:id'): ${err}`);
+        }
+    });
+
+    app.post('/api/workouts/', async (req, res) => {
+        try {
+            // Store the data type sent in the request
+            const { type } = req.body;
+            
+            // Create a new workout document using the mongoose model
+            const newWorkout = new db.Workout({
+                day: new Date(),
+                exercises: []
+            });
+            
+            // Store the data sent in the request body and feed into the newWorkout
+            if (type === "resistance") {
+                const { name, duration, weight, reps, sets } = req.body;
+                newWorkout.exercises = [{
+                    type,
+                    name,
+                    duration,
+                    weight,
+                    reps,
+                    sets
+                }];
+            } else if (type === "cardio") {
+                const { name, distance, duration } = req.body;
+                newWorkout.exercises = [{
+                    type,
+                    name,
+                    distance,
+                    duration
+                }];
+            }
+
+            // Save the new workout to the database
+            const addedWorkout = await newWorkout.save();
+
+            // If no workout added, throw a 501 error
+            if (!addedWorkout) throw new Error('501: Workout not added');
+ 
+            // Return the workouts to the user
+            return res.json(addedWorkout);
+
+        } catch (err) {
+            console.error(`ERROR - api-routes.js - .post('/api/workouts/'): ${err}`);
         }
     });
 }
